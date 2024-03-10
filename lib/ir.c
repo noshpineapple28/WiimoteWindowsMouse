1511730aebba94df840867d84e9e4933e711a223
noshpineapple28 <manoucherin@gmail.com>
2024-03-10 15:40:52 -0400

    feat: mouse movement now complete w/ sensor bar

--- lib/ir.c
+++ lib/ir.c
@@ -1,34 +1,34 @@
 /*
- *	wiiuse
+ *  wiiuse
  *
- *	Written By:
- *		Michael Laforest	< para >
- *		Email: < thepara (--AT--) g m a i l [--DOT--] com >
+ *  Written By:
+ *      Michael Laforest    < para >
+ *      Email: < thepara (--AT--) g m a i l [--DOT--] com >
  *
- *	Copyright 2006-2007
+ *  Copyright 2006-2007
  *
- *	This file is part of wiiuse.
+ *  This file is part of wiiuse.
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 3 of the License, or
- *	(at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *	$Header$
+ *  $Header$
  *
  */
 
 /**
- *	@file
- *	@brief Handles IR data.
+ *  @file
+ *  @brief Handles IR data.
  */
 
 #include "ir.h"
@@ -79,10 +79,10 @@ void wiiuse_set_ir_mode(struct wiimote_t *wm)
     wiiuse_write_data(wm, WM_REG_IR_MODENUM, &buf, 1);
 }
 /**
- *	@brief	Set if the wiimote should track IR targets.
+ *  @brief  Set if the wiimote should track IR targets.
  *
- *	@param wm		Pointer to a wiimote_t structure.
- *	@param status	1 to enable, 0 to disable.
+ *  @param wm       Pointer to a wiimote_t structure.
+ *  @param status   1 to enable, 0 to disable.
  */
 void wiiuse_set_ir(struct wiimote_t *wm, int status)
 {
@@ -97,10 +97,10 @@ void wiiuse_set_ir(struct wiimote_t *wm, int status)
     }
 
     /*
-     *	Wait for the handshake to finish first.
-     *	When it handshake finishes and sees that
-     *	IR is enabled, it will call this function
-     *	again to actually enable IR.
+     *  Wait for the handshake to finish first.
+     *  When it handshake finishes and sees that
+     *  IR is enabled, it will call this function
+     *  again to actually enable IR.
      */
     if (!WIIMOTE_IS_SET(wm, WIIMOTE_STATE_HANDSHAKE_COMPLETE))
     {
@@ -113,7 +113,7 @@ void wiiuse_set_ir(struct wiimote_t *wm, int status)
     }
 
     /*
-     *	Check to make sure a sensitivity setting is selected.
+     *  Check to make sure a sensitivity setting is selected.
      */
     ir_level = get_ir_sens(wm, &block1, &block2);
     if (!ir_level)
@@ -182,13 +182,13 @@ void wiiuse_set_ir(struct wiimote_t *wm, int status)
 }
 
 /**
- *	@brief	Get the IR sensitivity settings.
+ *  @brief  Get the IR sensitivity settings.
  *
- *	@param wm		Pointer to a wiimote_t structure.
- *	@param block1	[out] Pointer to where block1 will be set.
- *	@param block2	[out] Pointer to where block2 will be set.
+ *  @param wm       Pointer to a wiimote_t structure.
+ *  @param block1   [out] Pointer to where block1 will be set.
+ *  @param block2   [out] Pointer to where block2 will be set.
  *
- *	@return Returns the sensitivity level.
+ *  @return Returns the sensitivity level.
  */
 static int get_ir_sens(struct wiimote_t *wm, const byte **block1, const byte **block2)
 {
@@ -225,10 +225,10 @@ static int get_ir_sens(struct wiimote_t *wm, const byte **block1, const byte **b
 }
 
 /**
- *	@brief	Set the virtual screen resolution for IR tracking.
+ *  @brief  Set the virtual screen resolution for IR tracking.
  *
- *	@param wm		Pointer to a wiimote_t structure.
- *	@param status	1 to enable, 0 to disable.
+ *  @param wm       Pointer to a wiimote_t structure.
+ *  @param status   1 to enable, 0 to disable.
  */
 void wiiuse_set_ir_vres(struct wiimote_t *wm, unsigned int x, unsigned int y)
 {
@@ -242,9 +242,9 @@ void wiiuse_set_ir_vres(struct wiimote_t *wm, unsigned int x, unsigned int y)
 }
 
 /**
- *	@brief	Set the XY position for the IR cursor.
+ *  @brief  Set the XY position for the IR cursor.
  *
- *	@param wm		Pointer to a wiimote_t structure.
+ *  @param wm       Pointer to a wiimote_t structure.
  */
 void wiiuse_set_ir_position(struct wiimote_t *wm, enum ir_position_t pos)
 {
@@ -290,10 +290,10 @@ void wiiuse_set_ir_position(struct wiimote_t *wm, enum ir_position_t pos)
 }
 
 /**
- *	@brief	Set the aspect ratio of the TV/monitor.
+ *  @brief  Set the aspect ratio of the TV/monitor.
  *
- *	@param wm		Pointer to a wiimote_t structure.
- *	@param aspect	Either WIIUSE_ASPECT_16_9 or WIIUSE_ASPECT_4_3
+ *  @param wm       Pointer to a wiimote_t structure.
+ *  @param aspect   Either WIIUSE_ASPECT_16_9 or WIIUSE_ASPECT_4_3
  */
 void wiiuse_set_aspect_ratio(struct wiimote_t *wm, enum aspect_t aspect)
 {
@@ -319,13 +319,13 @@ void wiiuse_set_aspect_ratio(struct wiimote_t *wm, enum aspect_t aspect)
 }
 
 /**
- *	@brief	Set the IR sensitivity.
+ *  @brief  Set the IR sensitivity.
  *
- *	@param wm		Pointer to a wiimote_t structure.
- *	@param level	1-5, same as Wii system sensitivity setting.
+ *  @param wm       Pointer to a wiimote_t structure.
+ *  @param level    1-5, same as Wii system sensitivity setting.
  *
- *	If the level is < 1, then level will be set to 1.
- *	If the level is > 5, then level will be set to 5.
+ *  If the level is < 1, then level will be set to 1.
+ *  If the level is > 5, then level will be set to 5.
  */
 void wiiuse_set_ir_sensitivity(struct wiimote_t *wm, int level)
 {
@@ -381,10 +381,10 @@ void wiiuse_set_ir_sensitivity(struct wiimote_t *wm, int level)
 }
 
 /**
- *	@brief Calculate the data from the IR spots.  Basic IR mode.
+ *  @brief Calculate the data from the IR spots.  Basic IR mode.
  *
- *	@param wm		Pointer to a wiimote_t structure.
- *	@param data		Data returned by the wiimote for the IR spots.
+ *  @param wm       Pointer to a wiimote_t structure.
+ *  @param data     Data returned by the wiimote for the IR spots.
  */
 void calculate_basic_ir(struct wiimote_t *wm, byte *data)
 {
@@ -420,10 +420,10 @@ void calculate_basic_ir(struct wiimote_t *wm, byte *data)
 }
 
 /**
- *	@brief Calculate the data from the IR spots.  Extended IR mode.
+ *  @brief Calculate the data from the IR spots.  Extended IR mode.
  *
- *	@param wm		Pointer to a wiimote_t structure.
- *	@param data		Data returned by the wiimote for the IR spots.
+ *  @param wm       Pointer to a wiimote_t structure.
+ *  @param data     Data returned by the wiimote for the IR spots.
  */
 void calculate_extended_ir(struct wiimote_t *wm, byte *data)
 {
@@ -451,9 +451,9 @@ void calculate_extended_ir(struct wiimote_t *wm, byte *data)
 }
 
 /**
- *	@brief Interpret IR data into more user friendly variables.
+ *  @brief Interpret IR data into more user friendly variables.
  *
- *	@param wm		Pointer to a wiimote_t structure.
+ *  @param wm       Pointer to a wiimote_t structure.
  */
 static void interpret_ir_data(struct wiimote_t *wm)
 {
@@ -502,7 +502,7 @@ static void interpret_ir_data(struct wiimote_t *wm)
         if (wm->ir.state < 2)
         {
             /*
-             *	Only 1 known dot, so use just that.
+             *  Only 1 known dot, so use just that.
              */
             for (i = 0; i < 4; ++i)
             {
@@ -514,7 +514,7 @@ static void interpret_ir_data(struct wiimote_t *wm)
                     wm->ir.ax = wm->ir.x;
                     wm->ir.ay = wm->ir.y;
 
-                    /*	can't calculate yaw because we don't have the distance */
+                    /*  can't calculate yaw because we don't have the distance */
                     /* wm->orient.yaw = calc_yaw(&wm->ir); */
 
                     ir_convert_to_vres(&wm->ir.x, &wm->ir.y, wm->ir.aspect, wm->ir.vres[0], wm->ir.vres[1]);
@@ -524,9 +524,9 @@ static void interpret_ir_data(struct wiimote_t *wm)
         } else
         {
             /*
-             *	Only see 1 dot but know theres 2.
-             *	Try to estimate where the other one
-             *	should be and use that.
+             *  Only see 1 dot but know theres 2.
+             *  Try to estimate where the other one
+             *  should be and use that.
              */
             for (i = 0; i < 4; ++i)
             {
@@ -571,8 +571,8 @@ static void interpret_ir_data(struct wiimote_t *wm)
     case 4:
     {
         /*
-         *	Two (or more) dots known and seen.
-         *	Average them together to estimate the true location.
+         *  Two (or more) dots known and seen.
+         *  Average them together to estimate the true location.
          */
         int x, y;
         wm->ir.state = 2;
@@ -629,18 +629,18 @@ static void interpret_ir_data(struct wiimote_t *wm)
 }
 
 /**
- *	@brief Fix the rotation of the IR dots.
+ *  @brief Fix the rotation of the IR dots.
  *
- *	@param dot		An array of 4 ir_dot_t objects.
- *	@param ang		The roll angle to correct by (-180, 180)
+ *  @param dot      An array of 4 ir_dot_t objects.
+ *  @param ang      The roll angle to correct by (-180, 180)
  *
- *	If there is roll then the dots are rotated
- *	around the origin and give a false cursor
- *	position. Correct for the roll.
+ *  If there is roll then the dots are rotated
+ *  around the origin and give a false cursor
+ *  position. Correct for the roll.
  *
- *	If the accelerometer is off then obviously
- *	this will not do anything and the cursor
- *	position may be inaccurate.
+ *  If the accelerometer is off then obviously
+ *  this will not do anything and the cursor
+ *  position may be inaccurate.
  */
 static void fix_rotated_ir_dots(struct ir_dot_t *dot, float ang)
 {
@@ -662,8 +662,8 @@ static void fix_rotated_ir_dots(struct ir_dot_t *dot, float ang)
     c = cosf(DEGREE_TO_RAD(ang));
 
     /*
-     *	[ cos(theta)  -sin(theta) ][ ir->rx ]
-     *	[ sin(theta)  cos(theta)  ][ ir->ry ]
+     *  [ cos(theta)  -sin(theta) ][ ir->rx ]
+     *  [ sin(theta)  cos(theta)  ][ ir->ry ]
      */
 
     for (i = 0; i < 4; ++i)
@@ -685,11 +685,11 @@ static void fix_rotated_ir_dots(struct ir_dot_t *dot, float ang)
 }
 
 /**
- *	@brief Average IR dots.
+ *  @brief Average IR dots.
  *
- *	@param dot		An array of 4 ir_dot_t objects.
- *	@param x		[out] Average X
- *	@param y		[out] Average Y
+ *  @param dot      An array of 4 ir_dot_t objects.
+ *  @param x        [out] Average X
+ *  @param y        [out] Average Y
  */
 static void get_ir_dot_avg(struct ir_dot_t *dot, int *x, int *y)
 {
@@ -713,9 +713,9 @@ static void get_ir_dot_avg(struct ir_dot_t *dot, int *x, int *y)
 }
 
 /**
- *	@brief Reorder the IR dots.
+ *  @brief Reorder the IR dots.
  *
- *	@param dot		An array of 4 ir_dot_t objects.
+ *  @param dot      An array of 4 ir_dot_t objects.
  */
 static void reorder_ir_dots(struct ir_dot_t *dot)
 {
@@ -750,9 +750,9 @@ static void reorder_ir_dots(struct ir_dot_t *dot)
 }
 
 /**
- *	@brief Calculate the distance between the first 2 visible IR dots.
+ *  @brief Calculate the distance between the first 2 visible IR dots.
  *
- *	@param dot		An array of 4 ir_dot_t objects.
+ *  @param dot      An array of 4 ir_dot_t objects.
  */
 static float ir_distance(struct ir_dot_t *dot)
 {
@@ -786,18 +786,18 @@ static float ir_distance(struct ir_dot_t *dot)
 }
 
 /**
- *	@brief Correct for the IR bounding box.
+ *  @brief Correct for the IR bounding box.
  *
- *	@param x		[out] The current X, it will be updated if valid.
- *	@param y		[out] The current Y, it will be updated if valid.
- *	@param aspect	Aspect ratio of the screen.
- *	@param offset_x	The X offset of the bounding box.
- *	@param offset_y	The Y offset of the bounding box.
+ *  @param x        [out] The current X, it will be updated if valid.
+ *  @param y        [out] The current Y, it will be updated if valid.
+ *  @param aspect   Aspect ratio of the screen.
+ *  @param offset_x The X offset of the bounding box.
+ *  @param offset_y The Y offset of the bounding box.
  *
- *	@return Returns 1 if the point is valid and was updated.
+ *  @return Returns 1 if the point is valid and was updated.
  *
- *	Nintendo was smart with this bit. They sacrifice a little
- *	precision for a big increase in usability.
+ *  Nintendo was smart with this bit. They sacrifice a little
+ *  precision for a big increase in usability.
  */
 static int ir_correct_for_bounds(int *x, int *y, enum aspect_t aspect, int offset_x, int offset_y)
 {
@@ -829,7 +829,7 @@ static int ir_correct_for_bounds(int *x, int *y, enum aspect_t aspect, int offse
 }
 
 /**
- *	@brief Interpolate the point to the user defined virtual screen resolution.
+ *  @brief Interpolate the point to the user defined virtual screen resolution.
  */
 static void ir_convert_to_vres(int *x, int *y, enum aspect_t aspect, int vx, int vy)
 {
@@ -853,9 +853,9 @@ static void ir_convert_to_vres(int *x, int *y, enum aspect_t aspect, int vx, int
 }
 
 /**
- *	@brief Calculate yaw given the IR data.
+ *  @brief Calculate yaw given the IR data.
  *
- *	@param ir	IR data structure.
+ *  @param ir   IR data structure.
  */
 float calc_yaw(struct ir_t *ir)
 {
--- src/main.c
+++ src/main.c
@@ -10,11 +10,13 @@
 // wiiuse
 #include "wiiuse.h"
 
-int get_pos(POINT* cursor) {
+int get_pos(POINT *cursor)
+{
     return GetCursorPos(cursor);
 }
 
-void update_mouse_button(INPUT* input) {
+void update_mouse_button(INPUT *input)
+{
     // get mouse pos
     POINT cursor;
     get_pos(&cursor);
@@ -22,67 +24,159 @@ void update_mouse_button(INPUT* input) {
     input->type = INPUT_MOUSE;
     input->mi.dx = cursor.x;
     input->mi.dy = cursor.y;
+    input->mi.time = 0;
     SendInput(1, input, sizeof(INPUT));
 }
 
-void left_mouse_down() {
+void left_mouse_down()
+{
     INPUT input;
     input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
     update_mouse_button(&input);
 }
 
-void left_mouse_up() {
+void left_mouse_up()
+{
     INPUT input;
     input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
     update_mouse_button(&input);
 }
 
-void right_mouse_down() {
+void right_mouse_down()
+{
     INPUT input;
     input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
     update_mouse_button(&input);
 }
 
-void right_mouse_up() {
+void right_mouse_up()
+{
     INPUT input;
     input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
     update_mouse_button(&input);
 }
 
-void move_mouse(int dx, int dy) {
+void move_mouse(int dx, int dy)
+{
     // mouse move input
-    INPUT input;
-    input.mi.dwFlags = MOUSEEVENTF_MOVE;
-    // get mouse pos
-    POINT cursor;
-    get_pos(&cursor);
+    if (dx >= 4000000)
+        dx = 0;
 
-    input.type = INPUT_MOUSE;
-    input.mi.dx = dx;
-    input.mi.dy = dy;
-    SendInput(1, &input, sizeof(INPUT));
+    if (dy >= 4000000)
+        dy = 0;
+
+    // printf("x: %u, y: %u\t", dx, dy);
+    SetCursorPos(dx, dy);
 }
 
-int main() {
-    // connect wiimotes
-    wiimote** wiimotes_found;
+wiimote *setup_remote()
+{
+    wiimote **wiimotes_found;
     int found, connected;
     wiimotes_found = wiiuse_init(1);
-    // found = wiiuse_find(wiimotes_found, 1, 5);
+    found = wiiuse_find(wiimotes_found, 1, 5);
 
-    // if (!found) {
-    //     printf("NO WIIMOTES FOUND\n");
-    //     return 0;
-    // }
+    if (!found)
+    {
+        printf("NO WIIMOTES FOUND\n");
+        return 0;
+    }
 
-    // // set wiimotes
-    // wiimote* remote = wiimotes_found[0];
-    // connected = wiiuse_connect(&remote, 1);
-    // if (!connected) {
-    //     printf("Failed to connect to remote\n");
-    //     return 0;
-    // }
-    // wiiuse_set_leds(remote, 0x00);
+    // set wiimotes
+    connected = wiiuse_connect(wiimotes_found, 1);
+    if (!connected)
+    {
+        printf("Failed to connect to remote\n");
+        return 0;
+    }
+    wiimote *remote = wiimotes_found[0];
+    wiiuse_set_leds(remote, 0x00);
+
+    return remote;
+}
+
+int handle_input(wiimote *remote)
+{
+    if (IS_JUST_PRESSED(remote, WIIMOTE_BUTTON_A))
+    {
+        left_mouse_down();
+    }
+    else if (IS_RELEASED(remote, WIIMOTE_BUTTON_A))
+    {
+        left_mouse_up();
+    }
+    if (IS_JUST_PRESSED(remote, WIIMOTE_BUTTON_B))
+    {
+        right_mouse_down();
+    }
+    else if (IS_RELEASED(remote, WIIMOTE_BUTTON_B))
+    {
+        right_mouse_up();
+    }
+    if (IS_PRESSED(remote, 0x0010))
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+short any_wiimote_connected(wiimote *wm, int wiimotes)
+{
+    if (!wm)
+    {
+        return 0;
+    }
+    if (wm && WIIMOTE_IS_CONNECTED(wm))
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+void read_remote(wiimote **remote)
+{
+    wiiuse_set_ir(remote[0], 1);
+    wiiuse_motion_sensing(remote[0], 1);
+    wiiuse_set_ir_vres(remote[0], 1920, 1080);
+    while (any_wiimote_connected(remote[0], 1))
+    {
+        if (wiiuse_poll(remote, 1))
+        {
+            /*
+             *  This happens if something happened on any wiimote.
+             *  So go through each one and check if anything happened.
+             */
+            switch (remote[0]->event)
+            {
+            case WIIUSE_EVENT:
+                /* a generic event occurred */
+                if (handle_input(remote[0]))
+                    return;
+                break;
+
+            default:
+                break;
+            }
+
+            // handle movement
+            int dx = remote[0]->ir.x;
+            int dy = remote[0]->ir.y;
+            move_mouse((int)dx, (int)dy);
+        }
+    }
+}
+
+int main()
+{
+    // connect wiimotes
+    wiimote *remote = setup_remote();
+    wiiuse_rumble(remote, 1);
+    Sleep(200);
+    wiiuse_rumble(remote, 0);
+    read_remote(&remote);
+    wiiuse_cleanup(&remote, 1);
 
     return 1;
 }
